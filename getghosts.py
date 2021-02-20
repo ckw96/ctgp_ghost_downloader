50c9058f41da2f4dcfec32fd0a7fd4eb9830d568
campbell <ckwang96@gmail.com>
2021-02-20 14:25:29 +1100

    limit to only retrieving best ghost in course data

--- getghosts.py
+++ getghosts.py
@@ -50,40 +50,40 @@ else:
 
 # Pick if you want nintendo/ctgp/200cc nintendo/200cc ctgp ghosts
 with urllib.request.urlopen(leaderboard) as url:
-    data = json.loads(url.read().decode()[1:])
+	data = json.loads(url.read().decode()[1:])
+print(data)
 
 for course_data in data['leaderboards']:
 	# Some errors(?) in database
-    if 'name' not in course_data.keys():
-        continue
+	if 'name' not in course_data.keys():
+		continue
 	
-    try:
-        print('-----------------------')
-        print(course_data['name'])
-        print('Retrieving course data')
+	try:
+		print('-----------------------')
+		print(course_data['name'])
+		print('Retrieving course data')
 		
 		# Data for course (want list of ghosts)
-        with urllib.request.urlopen(base_url + course_data['_links']['item']['href']) as url:
-            curr_course = json.loads(url.read().decode())
-        print('Retrieving best ghost data')
+		with urllib.request.urlopen(base_url + course_data['_links']['item']['href']+'?start=0&limit=1') as url:
+			curr_course = json.loads(url.read().decode())
+		print('Retrieving ghost data')
 		
 		# Get JSON data of ghost
-        with urllib.request.urlopen(base_url + curr_course['ghosts'][0]['_links']['item']['href']) as url:
-            stuff = json.loads(url.read().decode()[1:])
-        print('Generating file name')
-        time_components = re.split(':|\.', stuff['finishTime'])
-        filename = 'ghosts/'+stuff['trackName']+' SLOT'+("0x%X" % stuff['slotId'])[2:].zfill(2)+'-'+stuff['trackId']+'/'+time_components[0]+'m'+time_components[1]+'s'+time_components[2][:7]+' '+stuff['player']+'.rkg'
-        filename = filename.replace('>', '').replace('<', '').replace('*', '').replace(':', '')
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        print('Retrieving ghost file')
+		with urllib.request.urlopen(base_url + curr_course['ghosts'][0]['_links']['item']['href']) as url:
+			stuff = json.loads(url.read().decode()[1:])
+		time_components = re.split(':|\.', stuff['finishTime'])
+		filename = 'ghosts/'+stuff['trackName']+' SLOT'+("0x%X" % stuff['slotId'])[2:].zfill(2)+'-'+stuff['trackId']+'/'+time_components[0]+'m'+time_components[1]+'s'+time_components[2][:7]+' '+stuff['player']+'.rkg'
+		filename = filename.replace('>', '').replace('<', '').replace('*', '').replace(':', '')
+		os.makedirs(os.path.dirname(filename), exist_ok=True)
+		print('Retrieving ghost file')
 		
 		# Get ghost file
-        with urllib.request.urlopen(base_url + stuff['href']) as response, open(filename, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-        print()
-    except:
-        print('Failed. See log file for details.')
-        logging.exception('Got exception for {}'.format(course_data['name']))
-        with open(LOG_FILENAME, 'a') as f:
-            f.write('\n')
+		with urllib.request.urlopen(base_url + stuff['href']) as response, open(filename, 'wb') as out_file:
+			shutil.copyfileobj(response, out_file)
+		print()
+	except:
+		print('Failed. See log file for details.')
+		logging.exception('Got exception for {}'.format(course_data['name']))
+		with open(LOG_FILENAME, 'a') as f:
+			f.write('\n')
 print('\n-----------------------\nFinished\n-----------------------')
